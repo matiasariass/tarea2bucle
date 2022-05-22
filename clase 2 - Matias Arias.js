90fdcb936340009c301cd35a25a0b1a0ded977fa
matiasariass <105993400+matiasariass@users.noreply.github.com>
2022-05-22 17:10:09 -0300

    Update clase 2 - Matias Arias.js

--- clase 2 - Matias Arias.js
+++ clase 2 - Matias Arias.js
@@ -6,7 +6,7 @@ for(let i = 0; i < numero; i++){
     if(numero == 6){
         console.log("Hola, soy el numero 6")
     } else{
-        alert("El" + numero + "ingresado es incorrecto")
+        alert("El " + numero + " ingresado es incorrecto")
         console.log("ERRRRRRRRROR")
     }
 }
