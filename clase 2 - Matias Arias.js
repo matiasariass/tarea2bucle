c5a1ed60a0f03cdf9b1591a563db5c8006c6c3be
matiasariass <105993400+matiasariass@users.noreply.github.com>
2022-05-22 17:05:37 -0300

    Update clase 2 - Matias Arias.js

--- clase 2 - Matias Arias.js
+++ clase 2 - Matias Arias.js
@@ -2,4 +2,11 @@ debugger
 let numero = parseInt(prompt("Ingresar Numero de repeticiones"))
 for(let i = 0; i < numero; i++){
     console.log("hola")
+
+    if(numero == 6){
+        console.log("Hola, soy el numero 6")
+    } else{
+        alert("El" + numero + "ingresado es incorrecto")
+        console.log("ERRRRRRRRROR")
     }
+}
